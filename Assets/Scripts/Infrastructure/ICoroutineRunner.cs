3f6b4673e3c77201c4cb862ce1b6eaa9256e6f8f
PowGrow <slideb910@gmail.com>
2023-08-07 19:34:47 +0300

    Added SceneLoader, ICoroutineRunner

--- Assets/Scripts/Infrastructure/ICoroutineRunner.cs
+++ Assets/Scripts/Infrastructure/ICoroutineRunner.cs
@@ -1,6 +1,10 @@
-﻿namespace Pewpew.Infrastructure
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Pewpew.Infrastructure
 {
     public interface ICoroutineRunner
     {
+        public Coroutine StartCoroutine(IEnumerator coroutine);
     }
 }
