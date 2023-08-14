51de5106984bd93713bb3e4757af6795f14cc08e
PowGrow <slideb910@gmail.com>
2023-08-14 20:29:46 +0300

    Added asteroid prefab

---
+++ Assets/Scripts/Logic/Asteroid.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer MeshRenderer;
+    [SerializeField]
+    private ParticleSystem DestroyParticles;
+    [SerializeField]
+    private ParticleSystem DamageParticles;
+
+    [SerializeField]
+    [Range(0, 5)]
+    private int Health;
+
+    [ContextMenu("TestDamage")]
+    public void TestDamage()
+    {
+        GetDamage(1);
+    }
+
+    public void GetDamage(int damageValue)
+    {
+        Health -= damageValue;
+        DamageParticles.Play();
+        if (Health <= 0)
+            Destruct();
+    }
+
+    private void Destruct()
+    {
+        DestroyParticles.Play();
+        StartCoroutine(HideAsteroid(0.5f));
+        StartCoroutine(DestoryAsteroid(1.5f));
+    }
+
+    private IEnumerator DestoryAsteroid(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator HideAsteroid(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        MeshRenderer.enabled = false;
+    }
+}
